f8bdc29ed5354cbd7a712a70eb09b276854d2063
Bogdan Gherghina <bogdan.gherghina@bearingpoint.com>
2021-08-16 23:06:44 +0300

    Enabled CORS

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Depends, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 from sqlalchemy.orm import Session
 
@@ -14,6 +15,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"],
+                   allow_headers=["*"])
+
 model = load_model('../Clasificatoare/Clasificator_Wild')
 
 
@@ -26,36 +30,36 @@ def get_db():
         db.close()
 
 
-@app.get('/')
+@ app.get('/')
 def hello_world():
     return{'hello': 'world'}
 
 
-@app.get('/public-places/', response_model=List[schemas.PublicPlace])
+@ app.get('/public-places/', response_model=List[schemas.PublicPlace])
 def get_public_places(db: Session = Depends(get_db)):
     public_places = crud.get_public_places(db=db)
     return public_places
 
 
-@app.get('/public-places/{public_place_id}', response_model=schemas.PublicPlace)
+@ app.get('/public-places/{public_place_id}', response_model=schemas.PublicPlace)
 def get_public_place(public_place_id: int, db: Session = Depends(get_db)):
     db_public_place = crud.get_public_place(
         db=db, public_place_id=public_place_id)
     return db_public_place
 
 
-@app.post('/public-places/', response_model=schemas.PublicPlace)
+@ app.post('/public-places/', response_model=schemas.PublicPlace)
 def create_public_place(public_place: schemas.PublicPlaceCreate, db: Session = Depends(get_db)):
     return crud.create_public_place(db=db, public_place=public_place)
 
 
-@app.post('/public-places/{public_place_id}/reports', response_model=schemas.Report)
+@ app.post('/public-places/{public_place_id}/reports', response_model=schemas.Report)
 async def create_report(public_place_id: int, db: Session = Depends(get_db),
                         file: UploadFile = File(...)):
     return await crud.create_report(db=db, public_place_id=public_place_id,
                                     file=file, model=model)
 
 
-@app.delete('/public-places/reports/{report_id}/', response_model=schemas.Report)
+@ app.delete('/public-places/reports/{report_id}/', response_model=schemas.Report)
 def delete_report(report_id: int, db: Session = Depends(get_db)):
     return crud.delete_report(db=db, report_id=report_id)
